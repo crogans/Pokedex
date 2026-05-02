de9bd3a6c46c266b858ad86d49384ba111aad508
crogans <cerysrogan@gmail.com>
2026-05-02 12:46:34 -0600

    Added types and started basic CSS styling

--- js/main.js
+++ js/main.js
@@ -5,20 +5,27 @@ const pokemonList = await getPokemonList()
 
 // Get the details of each Pokemon in the list
 const pokemonDetails = await Promise.all(
-  pokemonList.map(pokemon => getPokemon(pokemon.name))
+    pokemonList.map(pokemon => getPokemon(pokemon.name))
 )
 
 const pokemonContainer = document.getElementById("pokemon")
 
 // Displays the Pokemon details on the page
 pokemonDetails.forEach(pokemon => {
-  const pokemonCard = document.createElement("div")
-  pokemonCard.classList.add("pokemon-card")
+    const pokemonCard = document.createElement("div")
+    pokemonCard.classList.add("pokemon-card")
 
-  pokemonCard.innerHTML = `
-    <h3>#${pokemon.id} ${pokemon.name}</h3>
+    // Get the types of the Pokemon and create labels for them
+    const types = pokemon.types.map(t => t.type.name)
+    const typeLabels = types.map(type => `<span class="type ${type}">${type}</span>`).join("")
+
+    pokemonCard.innerHTML = `
+    <h3>${pokemon.name}</h3>
+    <div class="types">
+      ${typeLabels}
+    </div>
     <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
   `
 
-  pokemonContainer.appendChild(pokemonCard)
+    pokemonContainer.appendChild(pokemonCard)
 })
