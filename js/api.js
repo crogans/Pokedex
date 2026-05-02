50a9f19533d7648fcadef494b35c1d7579b8ab2c
crogans <cerysrogan@gmail.com>
2026-05-02 11:28:11 -0600

    Created pokemonContainer and pokemonCard that renders the API data

--- js/api.js
+++ js/api.js
@@ -1,9 +1,17 @@
 const BASE_URL = "https://pokeapi.co/api/v2"
 
+// Get a single Pokemon by name
 const getPokemon = async (name) => {
   let response = await fetch(`${BASE_URL}/pokemon/${name}`)
   let data = await response.json()
   return data
 }
 
-export { getPokemon }
+// Get a list of all Pokemon that exist (1025 with Gen 9)
+const getPokemonList = async () => {
+  let response = await fetch(`${BASE_URL}/pokemon?limit=1025`);
+  let data = await response.json();
+  return data.results;
+};
+
+export { getPokemon, getPokemonList }
--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
-import { getPokemon } from "./api.js";
+import { getPokemon, getPokemonList } from "./api.js"
 
-const data = await getPokemon("pikachu");
-console.log(data);
+// Get the list of all Pokemon
+const pokemonList = await getPokemonList()
+
+// Get the details of each Pokemon in the list
+const pokemonDetails = await Promise.all(
+  pokemonList.map(pokemon => getPokemon(pokemon.name))
+)
+
+const pokemonContainer = document.getElementById("pokemon")
+
+// Displays the Pokemon details on the page
+pokemonDetails.forEach(pokemon => {
+  const pokemonCard = document.createElement("div")
+  pokemonCard.classList.add("pokemon-card")
+
+  pokemonCard.innerHTML = `
+    <h3>#${pokemon.id} ${pokemon.name}</h3>
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+  `
+
+  pokemonContainer.appendChild(pokemonCard)
+})
